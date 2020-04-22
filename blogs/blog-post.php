ab04d4a3218ccc7f74f79f5b16598625d11bdd3f
kugayamashusei <buihuyhoang9a3@gmail.com>
2020-04-22 20:20:02 +0700

    push cơ sở dữ liệu

--- blogs/blog-post.php
+++ blogs/blog-post.php
@@ -52,28 +52,21 @@
                     <div class="section-row sticky-container">
                         <div class="main-post">
                             <h3><?=$post_blog['title']?></h3>
+                            <img class="img-responsive" src="./img/<?php echo $post_blog['thumbnail']; ?>" alt="">
+
                             <p><?=$post_blog['description']?></p>
                             <p><?=$post_blog['contents']?></p>
                             <p><b><i>Đăng bởi: <?=$post_blog['NAME']?></i></b></p>
-
                         </div>
-                        <div class="post-shares sticky-shares">
-                            <a href="#" class="share-facebook"><i class="fa fa-facebook"></i></a>
-                            <a href="#" class="share-twitter"><i class="fa fa-twitter"></i></a>
-                            <a href="#" class="share-google-plus"><i class="fa fa-google-plus"></i></a>
-                            <a href="#" class="share-pinterest"><i class="fa fa-pinterest"></i></a>
-                            <a href="#" class="share-linkedin"><i class="fa fa-linkedin"></i></a>
-                            <a href="#"><i class="fa fa-envelope"></i></a>
+                        <div class=" post-shares sticky-shares">
+                            <a href=" https://www.facebook.com/theskinfood.us" class="share-facebook"><i
+                                    class="fa fa-facebook"></i></a>
+                            <a href="https://twitter.com/skinfood_us" class="share-twitter"><i
+                                    class="fa fa-twitter"></i></a>
+                            <a href="https://www.instagram.com/skinfood_us/" class="share-pinterest"><i
+                                    class="fa fa-instagram"></i></a>
                         </div>
                     </div>
-
-                    <!-- ad -->
-                    <div class="section-row text-center">
-                        <a href="#" style="display: inline-block;margin: auto;">
-                            <img class="img-responsive" src="./img/ad-2.jpg" alt="">
-                        </a>
-                    </div>
-                    <!-- ad -->
 
                     <!-- author -->
                     <div class="section-row">
